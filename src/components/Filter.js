75ca42d38c030277f86517eb1b29e8c0d0539d07
Niharika Patil <niharikapatil04@gmail.com>
2021-05-17 14:49:56 +0530

    [Bug fix] Table header and rendering date was mismatch

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,37 +1,34 @@
-import React, { useState, useCallback, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
 
-import SlotsList from './SlotsList';
-import { useDistricts } from '../hooks/useDistricts';
+import SlotsList from "./SlotsList";
+import { useDistricts } from "../hooks/useDistricts";
+import useAppointments from "../hooks/useAppointments";
 
-const Filter = props => {
+const Filter = (props) => {
     const [
-        states, 
-        districts, 
-        selectedState, 
+        states,
+        districts,
+        selectedState,
         selectedDistrict,
-        appointmentByDistricts,
-        setSelectedState, 
+        setSelectedState,
         setSelectedDistrict,
-        setAppointmentByDistricts
     ] = useDistricts();
 
+    const [appointmentByDistricts] = useAppointments([selectedDistrict]);
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
     const [applyIsAvailable, setApplyIsAvailable] = useState(false);
     const [applyEighteenPlus, setapplyEighteenPlus] = useState(false);
-    //const [applyEighteenPlus, setapplyEighteenPlus] = useState(false);
 
     function handleApplyIsAvailable(event) {
-        if(!applyIsAvailable){
+        if (!applyIsAvailable) {
             setApplyIsAvailable(true);
-        } else
-        setApplyIsAvailable(false);
+        } else setApplyIsAvailable(false);
     }
 
     function handleApplyEighteenPlus(event) {
-        if(!applyEighteenPlus){
+        if (!applyEighteenPlus) {
             setapplyEighteenPlus(true);
-        } else
-        setapplyEighteenPlus(false);
+        } else setapplyEighteenPlus(false);
     }
 
     return (
@@ -40,58 +37,74 @@ const Filter = props => {
                 <select
                     className="dropdown-style"
                     value={selectedState}
-                    onChange={e => {
-                        setSelectedState(e.target.value)
-                    }}>
+                    onChange={(e) => {
+                        setSelectedState(e.target.value);
+                    }}
+                >
                     <option>-- Select State --</option>
-                    {
-                        states.map(state =>
-                            <option
-                                value={state.state_id}
-                                key={state.state_id} >
-                                {state.state_name}
-                            </option>
-                    )}
+                    {states.map((state) => (
+                        <option value={state.state_id} key={state.state_id}>
+                            {state.state_name}
+                        </option>
+                    ))}
                 </select>
                 <select
                     className="dropdown-style"
                     value={selectedDistrict}
-                    onChange={e => {
+                    onChange={(e) => {
                         setSelectedDistrict(e.target.value);
-                    }}>
+                    }}
+                >
                     <option>-- Select District --</option>
-                    {
-                        districts.map(district =>
-                            <option
-                                value={district.district_id}
-                                key={district.district_id} >
-                                {district.district_name}
-                            </option>
-                        )}
+                    {districts.map((district) => (
+                        <option
+                            value={district.district_id}
+                            key={district.district_id}
+                        >
+                            {district.district_name}
+                        </option>
+                    ))}
                 </select>
             </div>
             <hr></hr>
             <div className="d-flex justify-content-end m-2">
-                <a href="#" >
-                    <span   className={`badge p-1 m-1 ${applyIsAvailable ? "badge-primary": "bg-light text-dark"}`}
-                            value={applyIsAvailable}
-                            onClick={handleApplyIsAvailable}
-                            >Available</span>
+                <a href="#">
+                    <span
+                        className={`badge p-1 m-1 ${
+                            applyIsAvailable
+                                ? "badge-primary"
+                                : "bg-light text-dark"
+                        }`}
+                        value={applyIsAvailable}
+                        onClick={handleApplyIsAvailable}
+                    >
+                        Available
+                    </span>
                 </a>
-                <a href="#" >
-                    <span   className={`badge p-1 m-1 ${applyEighteenPlus ? "badge-primary" : "bg-light text-dark"}`}
-                            value={applyEighteenPlus}
-                            onClick={handleApplyEighteenPlus}>Age 18+</span>
+                <a href="#">
+                    <span
+                        className={`badge p-1 m-1 ${
+                            applyEighteenPlus
+                                ? "badge-primary"
+                                : "bg-light text-dark"
+                        }`}
+                        value={applyEighteenPlus}
+                        onClick={handleApplyEighteenPlus}
+                    >
+                        Age 18+
+                    </span>
                 </a>
             </div>
-                    {
-                        selectedDistrict ?
-                            <SlotsList  applyIsAvailable={applyIsAvailable}
-                                        applyEighteenPlus={applyEighteenPlus}
-                                        appointments={appointmentByDistricts}/>
-                            : <h6>Please select State and district</h6>
-                    }   
+            {selectedDistrict!=""? (
+                <SlotsList
+                    applyIsAvailable={applyIsAvailable}
+                    applyEighteenPlus={applyEighteenPlus}
+                    appointments={appointmentByDistricts}
+                />
+            ) : (
+                <h6>Please select State and district</h6>
+            )}
         </div>
     );
-}
+};
 export default Filter;
--- src/hooks/useDistricts.js
+++ src/hooks/useDistricts.js
@@ -1,92 +1,62 @@
-import { useEffect, useCallback, useState } from 'react';
+import { useEffect, useCallback, useState } from "react";
 import axios from "axios";
-
 
 export const useDistricts = (props) => {
     const [selectedState, setSelectedState] = useState("");
     const [selectedDistrict, setSelectedDistrict] = useState("");
     const [districts, setDistricts] = useState([]);
     const [states, setStates] = useState([]);
-    const [appointmentByDistricts, setAppointmentByDistricts] = useState([]);
 
     // to fetch states list and set in state
     const fetchStates = useCallback(() => {
-        //event.preventDefault();
         axios({
-        "method": "GET",
-        "url": "https://cdn-api.co-vin.in/api/v2/admin/location/states",
+            method: "GET",
+            url: "https://cdn-api.co-vin.in/api/v2/admin/location/states",
         })
-        .then((response) => {
-            setStates(response.data.states);  
+            .then((response) => {
+                setStates(response.data.states);
+            })
+            .catch((error) => {
+                setStates([]);
+                console.log(error);
+            });
+    }, []);
+
+    const fetchDistricts = useCallback(() => {
+        if (!selectedState) {
+            return;
+        }
+        axios({
+            method: "GET",
+            url: `https://cdn-api.co-vin.in/api/v2/admin/location/districts/${selectedState}`,
         })
-        .catch((error) => {
-            setStates([]);
-            console.log(error);
-        })
-    }, [])
+            .then((response) => {
+                setDistricts(response.data.districts);
+            })
+            .catch((error) => {
+                setDistricts([]);
+                console.log(error);
+            });
+    }, [selectedState]);
 
     // To fetch states when component is mounted
     useEffect(() => {
         fetchStates();
-    }, []);
+    },[fetchStates]);
 
-    const fetchDistricts = useCallback(() => {
-        
-        if(!selectedState) {
-            return;
-        }
-        axios({
-          "method": "GET",
-          "url": `https://cdn-api.co-vin.in/api/v2/admin/location/districts/${selectedState}`,
-        })
-        .then((response) => {
-            setDistricts(response.data.districts);  
-        })
-        .catch((error) => {
-            setDistricts([]);
-            console.log(error);
-        })
-    }, [selectedState])
-
+    // To fetch district list when selected state is changed
     useEffect(() => {
-        // fetch district list
         fetchDistricts();
     }, [selectedState]);
 
-    const fetchAppointmentByDistricts = useCallback(() => {
-        const currentDate = new Date().toISOString().slice(0, 10);
-        let newDate = currentDate.toString().split("-").reverse().join("-");
-        if(!selectedDistrict) {
-            return;
-        }
-        axios({
-          "method": "GET",
-          "url": `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=${selectedDistrict}&date=${newDate}`,
-        })
-        .then((response) => {
-            setAppointmentByDistricts(response.data.centers);
-        })
-        .catch((error) => {
-            setAppointmentByDistricts([]);
-            console.log(error);
-        })
-    }, [selectedDistrict])
-
-    useEffect(() => {
-        // fetch district list
-        fetchAppointmentByDistricts();
-    }, [selectedDistrict]);
-
     return [
-        states, 
-        districts, 
-        selectedState, 
+        states,
+        districts,
+        selectedState,
         selectedDistrict,
-        appointmentByDistricts,
-        setSelectedState, 
+        setSelectedState,
         setSelectedDistrict,
-        setAppointmentByDistricts
     ];
-}
+};
 
 export default useDistricts;
